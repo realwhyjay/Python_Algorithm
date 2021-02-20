e96756fce80804c7f57d368fab00e945e7f74a90
최영재 <brandon94_@naver.com>
2021-02-20 17:35:49 +0900

    baekjoon 4195 친구 ㅔ트워크

--- baekjoon/baekjoon 1920 수 찾기/1920.py
+++ baekjoon/baekjoon 1920 수 찾기/1920.py
@@ -1,7 +1,12 @@
-N = int(input())
-Nlist = list(map(int, input().split()))
-M = int(input())
-Mlist = list(map(int, input().split()))
+import sys
 
-for i in range(len(Mlist)):
-    print(1) if Nlist.count(Mlist[i]) >= 1 else print(0)
+N = int(sys.stdin.readline())
+Nset = set(map(int, sys.stdin.readline().split()))
+M = int(sys.stdin.readline())
+Mlist = list(map(int, sys.stdin.readline().split()))
+
+for i in Mlist:
+    if i in Nset:
+        print(1)
+    else:
+        print(0)
