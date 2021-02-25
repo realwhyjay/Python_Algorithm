db4beaf32a50553047d1d577650a2e663f2a6fc5
최영재 <brandon94_@naver.com>
2021-02-25 18:54:53 +0900

    baekjoon 10814 나이순 정렬

---
+++ baekjoon/baekjoon 10814 나이순 정렬/10814.py
@@ -0,0 +1,12 @@
+num = int(input())
+people = []
+
+for _ in range(num):
+    data = list(input().split())
+    people.append((int(data[0]), data[1]))
+
+people = sorted(people, key=lambda x: x[0])
+
+for i in people:
+    #print("{} {}".format(i[0], i[1]))
+    print(i[0], i[1])
---
+++ baekjoon/baekjoon 1427 소트인사이드/1427.py
@@ -0,0 +1,8 @@
+
+num_list = list(input())
+
+num_list.sort()
+num_list.reverse()
+
+for i in num_list:
+    print(i, end='')
