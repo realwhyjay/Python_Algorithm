680a9303ff3f0380792f5e4e92354f9ea8ac9608
최영재 <brandon94_@naver.com>
2021-03-06 01:26:38 +0900

    baekjoon 1668 트로피 진열

--- baekjoon/baekjoon 1668 트로피 진열/1668.py
+++ baekjoon/baekjoon 1668 트로피 진열/1668.py
@@ -1,3 +1,16 @@
+def trophy_count(trophys):
+    result = 0
+    for i in range(len(trophys)):
+        if i == 0:
+            result = 1
+            max = trophys[i]
+        else:
+            if trophys[i] > max:
+                result += 1
+                max = trophys[i]
+    return result
+
+
 num = int(input())
 trophy = []
 
@@ -6,27 +19,8 @@ for _ in range(num):
     trophy.append(int(input()))
 
 
-for i in range(len(trophy)):
-    if i == 0:
-        result = 1
-        max = trophy[i]
-    else:
-        if trophy[i] > max:
-            result += 1
-            max = trophy[i]
-
-
-print(result)
+print(trophy_count(trophy))
 
 trophy.reverse()
 
-for i in range(len(trophy)):
-    if i == 0:
-        result = 1
-        max = trophy[i]
-    else:
-        if trophy[i] > max:
-            result += 1
-            max = trophy[i]
-
-print(result)
+print(trophy_count(trophy))
