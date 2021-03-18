50ce00fb766b58c343e85659d0bb89e192bdaeea
최영재 <brandon94_@naver.com>
2021-03-18 15:57:45 +0900

    -

--- baekjoon/baekjoon 2512 예산/2512.py
+++ baekjoon/baekjoon 2512 예산/2512.py
@@ -5,8 +5,6 @@ limit = int(input())
 start = 1
 end = max(area)
 
-print("시작하자!! start : {} end : {} mid : {}".format(start, end, (start+end)//2))
-
 while start <= end:
     mid = (start+end)//2
     result = 0
@@ -21,7 +19,5 @@ while start <= end:
         end = mid - 1
     else:
         start = mid + 1
-    print("start : {} end : {} mid : {} result : {}".format(
-        start, end, mid, result))
 
 print(end)
