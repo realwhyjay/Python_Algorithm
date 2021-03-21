3dec702900c7f589efed64ad238110a80bdd239e
최영재 <brandon94_@naver.com>
2021-03-21 14:22:09 +0900

    이진 탐색

--- baekjoon/baekjoon 2343 기타 레슨/2343.py
+++ baekjoon/baekjoon 2343 기타 레슨/2343.py
@@ -1,18 +1,26 @@
 N, M = map(int, input().split())
 lesson = list(map(int, input().split()))
 
-start = 1
-end = max(lesson)
+start = max(lesson)
+end = sum(lesson)
 
 while start <= end:
     mid = (start + end)//2
     time = 0
-    count = 0
+    count = 1
 
     for i in lesson:
-        if i >= mid:
+        if time+1 >= mid:
             time = 0
             count += 1
         time += i
 
+    if time:
+        count += 1
+
     if count > M:
+        start = mid+1
+    else:
+        end = mid-1
+
+print(start)
