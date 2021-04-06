cf1750f7029e74fa93faabe64cd96508267b7a5c
최영재 <brandon94_@naver.com>
2021-04-06 20:56:07 +0900

    baekjoon 10972 다음 순열

---
+++ baekjoon/baekjoon 10972 다음 순열/10972.py
@@ -0,0 +1,33 @@
+num = int(input())
+a = list(map(int, input().split()))
+
+
+def next_permutations(a):
+    i = len(a)-1
+    # 끝에서부터 원소를 비교해주는데,
+    # 앞의 원소가 뒤의 원소보다 크다면 해당 원소의 값을 저장한다
+    while i > 0 and a[i-1] >= a[i]:
+        i -= 1
+    if i <= 0:
+        # 만일 end가 0이라면 완벽한 내림차순이기 때문에 False를 리턴 해준다.
+        return False
+
+    # a[i-1] 뒤의 값중 제일 a[i-1]보다 큰 값중 제일 작은 값을 확인하고 자리를 바꿔준다.
+    j = len(a)-1
+    while a[i-1] >= a[j]:
+        j -= 1
+    a[i-1], a[j] = a[j], a[i-1]
+
+    j = len(a)-1
+    while i < j:
+        a[i], a[j] = a[j], a[i]
+        i += 1
+        j -= 1
+
+    return True
+
+
+if next_permutations(a):
+    print(' '.join(map(str, a)))
+else:
+    print(-1)
--- baekjoon/baekjoon 6603 로또/6603.py
+++ baekjoon/baekjoon 6603 로또/6603.py
@@ -2,13 +2,10 @@ from itertools import combinations
 
 while True:
     test_case = list(map(int, input().split()))
-    print(test_case)
     if test_case[0] == 0:
-        print("호호")
         break
     num = test_case[0]
     lotto = test_case[1:]
-    print(num, lotto)
 
     result = list(combinations(lotto, 6))
 
