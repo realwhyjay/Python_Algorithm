f12d4926b520edc8d1be0d62f93e5e3da391b314
최영재 <brandon94_@naver.com>
2021-06-03 19:47:31 +0900

    baekjoon 10828 스택

---
+++ baekjoon/baekjoon 10828 스택/10828.py
@@ -0,0 +1,20 @@
+n = int(input())
+stack = []
+for _ in range(n):
+    command = list(input().split())
+    if command[0] == 'push':
+        stack.append(command[1])
+    elif command[0] == 'pop':
+        if not stack:
+            print(-1)
+        else:
+            print(stack.pop())
+    elif command[0] == 'top':
+        if not stack:
+            print(-1)
+        else:
+            print(stack[-1])
+    elif command[0] == 'size':
+        print(len(stack))
+    else:
+        print(1 if not stack else 0)
