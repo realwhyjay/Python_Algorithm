d0c72bf1647efd3d0d4be52f0f98c342977976c4
realwhyjay <brandon94_@naver.com>
2021-07-27 02:27:46 +0900

    baekjoon 10820 문자열 분석

--- baekjoon/baekjoon 10820 문자열 분석/10820.py
+++ baekjoon/baekjoon 10820 문자열 분석/10820.py
@@ -1,16 +1,18 @@
 
 while True:
-    str = list(input())
-    if not str:
+    try:
+        str = list(input())
+        low, upper, num, blank = 0, 0, 0, 0
+
+        for i in range(len(str)):
+            if str[i] == ' ':
+                blank += 1
+            elif ord('a') <= ord(str[i]) <= ord('z'):
+                low += 1
+            elif ord('A') <= ord(str[i]) <= ord('Z'):
+                upper += 1
+            elif 0 <= int(str[i]) <= 9:
+                num += 1
+        print(low, upper, num, blank)
+    except EOFError:
         break
-    low, upper, num, blank = 0, 0, 0, 0
-    for i in range(len(str)):
-        if str[i] == ' ':
-            blank += 1
-        elif ord('a') <= ord(str[i]) <= ord('z'):
-            low += 1
-        elif ord('A') <= ord(str[i]) <= ord('Z'):
-            upper += 1
-        elif 0 <= int(str[i]) <= 9:
-            num += 1
-    print(low, upper, num, blank)
