7a2fa3d10368fa07ab0b6ea1f88ab0eae3d96174
realwhyjay <brandon94_@naver.com>
2021-09-08 16:50:52 +0900

    baekjoon 11576 Base Conversion

---
+++ baekjoon/baekjoon 11576 Base Conversion/11576.py
@@ -0,0 +1,19 @@
+A, B = map(int, input().split())
+m = int(input())
+before = list(map(int, input().split()))
+temp = 0
+after = []
+before = before[::-1]
+
+for i in range(m):
+    temp += before[i]*(A**i)
+
+
+while (temp != 0):
+    print("temp : ", temp)
+    after.append(temp % B)
+    temp //= B
+
+after = after[::-1]
+for i in after:
+    print(i, end=" ")
